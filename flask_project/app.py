0472207ff3bb8ac481e1707fcd842bd3f5af874f
greenxlouv <Jesuisjaelin16@gmail.com>
2024-10-18 16:10:59 +0900

    초기작업

--- flask_project/app.py
+++ flask_project/app.py
@@ -3,6 +3,19 @@ import sys
 application = Flask(__name__)
 @application.route("/")
 def hello():
-    return "Hello my webpage!"
+    return render_template("index.html")
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', debug=True)
+    application.run(host='0.0.0.0')
+
+@application.route("/list")
+def view_list():
+    return render_template("list.html")
+@application.route("/review")
+def view_review():
+    return render_template("review.html")
+@application.route("/reg_items")
+def reg_item():
+    return render_template("reg_items.html")
+@application.route("/reg_reviews")
+def reg_review():
+    return render_template("reg_reviews.html")
